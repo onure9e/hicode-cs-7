9ccc1b551dfb28a88bd5ac1c7711c97c3185733f
Onurege <78586675+onure9e@users.noreply.github.com>
2022-12-04 07:04:20 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace while_forEach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("> bir sayı giriniz: ");
+            int sayi = int.Parse(Console.ReadLine());
+            int toplam = 0;
+            int sayac = 0;
+            while(sayac <=sayi){
+                toplam+=sayac;
+                sayac++;
+            }
+            Console.WriteLine(toplam/sayi);
+
+           string[] array = new String[6] {"Ankara","İstanbul","İzmir","Adana","Sivas","Hatay"};
+           foreach(var n in array){
+            Console.WriteLine(n);
+           }
+        }
+    }
+}
